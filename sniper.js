34cb17f926f9b0f47c3e4a9980256377e3cac456
Ubong James <ubjames001@gmail.com>
2024-08-07 18:22:33 +0100

    added version note at the top of the file

--- sniper.js
+++ sniper.js
@@ -13,6 +13,10 @@ See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
 
+/**
+ * sniper.js
+ * Version: 1.1.0
+ */
 
 /**
  * This function generates an array of years between a given start and end date.
